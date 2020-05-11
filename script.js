b3a59172ffe7dc3fe43634568134a1fc24a09cdb
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-05-11 18:35:08 -0500

    queryURL

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//#region
-var apiKey = "c9871eecb074811b7872bedc2fda20f7";
-//#endregion
 // $(document).ready(function() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&appid=" + apiKey;
-   // var city = "Houston"
-
+    var apiKey = "c9871eecb074811b7872bedc2fda20f7";
+    var city = ''
+    var unit = [];
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function(response) {
-        $('#test').html('<div>' + JSON.stringify(response));
+        $('#test').html('<div>' + (response.name));
         console.log(response);
     })
