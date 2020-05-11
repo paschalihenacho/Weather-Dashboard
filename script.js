1314eae6292d47cc0056e8c60f243ff06ed45f28
paschalihenacho <52731451+paschalihenacho@users.noreply.github.com>
2020-05-11 17:46:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//#region
-var apiKey = "c9871eecb074811b7872bedc2fda20f7";
-//#endregion
+
 // $(document).ready(function() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&appid=" + apiKey;
    // var city = "Houston"
