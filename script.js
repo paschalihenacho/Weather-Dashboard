82c98ec53bf879b5b6b3d685450d698554f9c5c6
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-05-13 05:52:27 -0500

    basic HTML

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1314eae6292d47cc0056e8c60f243ff06ed45f28
-// $(document).ready(function() {
+// SETUP VARIABLES
+// ================================================================
     var apiKey = "c9871eecb074811b7872bedc2fda20f7";
     var city = ''
-    var unit = [];
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    var queryURLBase = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=" + apiKey;
+    // `https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=${apiKey} &q=${city}`;
 
+// Functions
+// ================================================================
+function runQuery(oneDay) {
     $.ajax({
-        url: queryURL,
+        url: oneDay,
         method: "GET",
-    }).then(function(response) {
-        $('#test').html('<div>' + (response.name));
-        console.log(response);
+    }).then(function(weatherData) { 
+        $('#cityButtons').empty();
+        console.log(weatherData);
+        console.log(oneDay);
     })
+}    
+
+//    function renderCityBtn() {
+//         $('#cityButtons').empty();
+
+//         for (var i = 0; i < cities; i++) {
+//             var a = $('<button>');
+
+//             a.addClass('city-btn');
+
+//             a.attr('data-name', city[i]);
+
+//             a.text(cities[i]);
+
+//             $('#cityButtons').prepand(a)
+//         }
+//     }
+
+// MAIN PROCESS
+// ================================================================
+
+    $('#search').on('click', function (event) {
+        event.preventDefault();
+
+        city = $('#input').val().trim();
+
+        var newURL = queryURLBase + "&q=" + city;
+
+        var allCities = [];
+        
+        allCities = JSON.parse(localStorage.getItem("allCities")) || []; // Get cities
+        allCities.push(cityInput); // pushes new cities entered to array 
+        localStorage.setItem("allCities", JSON.stringify(allCities)); //saves city input to local storage 
+
+        // Add in the Search Term
+
+        runQuery(newURL);
+    });
+
+    // activity 7
+
+    
